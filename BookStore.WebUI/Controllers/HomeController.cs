3241c939b88c6b300db4aeabe1d07610391f64e1
AMR <amr@home.local>
2019-04-12 21:27:12 +0200

    Added Pagination List

--- BookStore.WebUI/Controllers/HomeController.cs
+++ BookStore.WebUI/Controllers/HomeController.cs
@@ -9,17 +9,31 @@ namespace BookStore.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        public int PageSize = 2;
         private IBookRepository repo;
         public HomeController(IBookRepository repoParam)
         {
             repo = repoParam;
         }
 
-        public ActionResult Index()
+        public ActionResult ListAll()
         {
 
             return View(repo.Books);
         }
+
+        public ActionResult List(int page=1)
+        {
+            //2-per Page
+            //(2-1)*2=2
+            //(3-1)*2=4
+            return View("listall",repo.Books
+                .OrderBy(b=>b.ISBN) 
+                .Skip((page-1)* PageSize)
+                .Take(PageSize)
+                );
+        }
+
 
         public ActionResult About()
         {
